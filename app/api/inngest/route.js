3c145937c41310bfb708ce1427b82474c59d832e
Philtah <philliprimayi01@gmail.com>
2025-08-29 12:26:06 +0200

    Refactor route.js to organize imports and remove unnecessary comments

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/config/inngest";
+import {
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "@/config/inngest";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
-
     syncUserCreation,
     syncUserUpdation,
     syncUserDeletion,
